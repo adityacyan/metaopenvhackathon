986333fca70a00eb5cf0c13c1c2142357d58039d
adityacyan <cntsgg@gmail.com>
2026-04-07 02:16:58 +0530

    modified:   inference.py

--- inference.py
+++ inference.py
@@ -68,6 +68,7 @@ USE_DOCKER = os.getenv("USE_DOCKER", "false").lower() == "true"  # Explicit Dock
 API_KEY = os.getenv("HF_TOKEN") or os.getenv("API_KEY") or os.getenv("OPENAI_API_KEY")
 API_BASE_URL = os.getenv("API_BASE_URL", "https://router.huggingface.co/v1")
 MODEL_NAME = os.getenv("MODEL_NAME", "Qwen/Qwen2.5-72B-Instruct")
+HF_TOKEN = os.getenv("HF_TOKEN")
 ENV_SERVER_URL = os.getenv(
     "ENV_SERVER_URL", "http://localhost:8000"
 )  # Local server URL
